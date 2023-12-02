15483088847213a220b5b7b04fa80ca3193386a1
Chinwilla <bogdandrozd123@gmail.com>
2023-12-01 23:17:29 -1200

    Update results

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,4 @@
-function makeTransition(quantity, pricePerDroid) {
+function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
